a45264ced5c82e872f132e3191dc20cf6ef8eeab
Kenenisa Beyan <kenenisab05@gmail.com>
2026-02-04 18:24:11 +0200

    Style todo app with clean CSS layout

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const taskList = document.getElementById("taskList");
 
 // Add click event
 addBtn.addEventListener("click", function () {
-  const taskText = taskInput.value;
+  const taskText = taskInput.value.trim();
+
+  // ❗ Prevent empty task
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
 
   const li = document.createElement("li");
   li.textContent = taskText;
@@ -13,4 +19,17 @@ addBtn.addEventListener("click", function () {
   taskList.appendChild(li);
 
   taskInput.value = "";
+});
+
+li.textContent = taskText;
+
+// Mark task as completed on click
+li.addEventListener("click", function () {
+  li.style.textDecoration = "line-through";
+});
+
+// Delete task on right click
+li.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+  taskList.removeChild(li);
 });
